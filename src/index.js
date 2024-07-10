408e0b459a94a5d04bddca812b4c1f5579808da6
Júlia Bacchi <134342345+juliabacchi1@users.noreply.github.com>
2024-07-10 14:39:30 -0300

    Added a plugin for generating recipe

--- src/index.js
+++ src/index.js
@@ -13,9 +13,6 @@ function displayRecipe(response) {
 function generateRecipe(event) {
   event.preventDefault();
 
-  let informationsElement = document.querySelector("#informations");
-  informationsElement.classList.remove("hidden");
-
   // API Url
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "4b59c6fof3bta7634229de85af0382e7";
@@ -23,6 +20,11 @@ function generateRecipe(event) {
   let context =
     "You are an intelligent culinary assistant called Kichen Assistant AI Generator. Your goal is to help users create delicious and creative recipes using the ingredients they have available at home. Users will provide the ingredients they have in their refrigerators, and your task is to generate a practical and tasty recipe using those ingredients. Please put a title recipe and separate each line and ingredient with a <br />. Sign the recipe with 'KA AI Generator' inside a <strong> element at the end of the recipe.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  // Hide the informations before the event click
+  let informationsElement = document.querySelector("#informations");
+  informationsElement.classList.remove("hidden");
+  informationsElement.innerHTML = `<div class="generating">⏳ Generating a Recipe </div>`;
 
   axios.get(apiUrl).then(displayRecipe);
 }
