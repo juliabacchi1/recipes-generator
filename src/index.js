43f7196e61e6a436554704eabbfcfb49b0932e1a
Júlia Bacchi <134342345+juliabacchi1@users.noreply.github.com>
2024-07-10 12:05:32 -0300

    Added some information to the IA, hidden the informations in JS before the click

--- src/index.js
+++ src/index.js
@@ -13,12 +13,15 @@ function displayRecipe(response) {
 function generateRecipe(event) {
   event.preventDefault();
 
+  let informationsElement = document.querySelector("#informations");
+  informationsElement.classList.remove("hidden");
+
   // API Url
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "4b59c6fof3bta7634229de85af0382e7";
   let prompt = `Create a delicious recipe using the following ingredients: ${instructionsInput.value}. Make sure the recipe is practical and tasty`;
   let context =
-    "You are an intelligent culinary assistant called Kichen Assistant AI Generator. Your goal is to help users create delicious and creative recipes using the ingredients they have available at home. Users will provide the ingredients they have in their refrigerators, and your task is to generate a practical and tasty recipe using those ingredients. Please separate each line and ingredient with a <br /> and Sign the recipe with 'KA AI Generator' inside a <strong> element at the end of the recipe.";
+    "You are an intelligent culinary assistant called Kichen Assistant AI Generator. Your goal is to help users create delicious and creative recipes using the ingredients they have available at home. Users will provide the ingredients they have in their refrigerators, and your task is to generate a practical and tasty recipe using those ingredients. Please put a title recipe and separate each line and ingredient with a <br />. Sign the recipe with 'KA AI Generator' inside a <strong> element at the end of the recipe.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayRecipe);
